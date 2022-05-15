ad956a5918c52dc90e96488a0e503784769ff117
Denis-Source <zolotoverkhdenis@gmail.com>
2022-05-15 12:56:21 +0300

    config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 
 class Config:
-    OPEN_WEATHER_API_KEY = "fdc4b4e12d2b09ba6e3efaf5f08669c3"
-    ACCUWEATHER_API_KEY = "6aLfMwtYXqyhy5g1fGFbG1TmoX2XuNAi"
+    OPEN_WEATHER_API_KEY = ""
+    ACCUWEATHER_API_KEY = ""
 
-    BG_COLOR_PRIMARY = "", "",
+    BG_COLOR_PRIMARY = "#bbb3d1", "#7b89cc",
     FG_COLOR_PRIMARY = "#FFFFFF",
 
     SUNSET_COLOR = "#e3a072", "#dd8078"
